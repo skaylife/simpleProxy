35904e94ea0a68d2f44079e6477eb3106c685858
skaylife <worldskaynet@gmail.com>
2025-06-04 02:46:22 +0700

    xi2

--- main.py
+++ main.py
@@ -19,24 +19,7 @@ USERNAME = "proxyuser"
 PASSWORD = "proxypass"
 
 # Middleware для проверки Basic Auth
-class AuthMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        auth = request.headers.get("authorization")
-        if not auth or not auth.startswith("Basic "):
-            raise HTTPException(status_code=401, detail="Unauthorized")
 
-        try:
-            credentials = base64.b64decode(auth.split(" ")[1]).decode("utf-8")
-            username, password = credentials.split(":")
-        except:
-            raise HTTPException(status_code=401, detail="Unauthorized")
-
-        if username != USERNAME or password != PASSWORD:
-            raise HTTPException(status_code=401, detail="Unauthorized")
-
-        return await call_next(request)
-
-app.add_middleware(AuthMiddleware)
 
 @app.get("/proxy")
 async def proxy(url: str):
